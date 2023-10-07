31c01c125e8af372b50eb2e5162c71e646e676f5
Cristian Dussán <cristian@sltwtr.com>
2023-10-07 16:56:08 -0500

    feature: add backend home carrousel

---
+++ assets/swiper.js
@@ -0,0 +1,13 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper-carrousel', {
+ 
+});
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></script>
